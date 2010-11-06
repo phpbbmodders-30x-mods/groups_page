125ae848d7d9cefc500909c2ccb2b9147eb44a95
tumba25 <tumba25@gmail.com>
2010-11-07 00:55:22 +0200

    Separate group leaders from mere mortals

--- root/groups.php
+++ root/groups.php
@@ -26,14 +26,14 @@ $user->setup('mods/groups');
 // this allows hidden groups to display
 $sql = 'SELECT group_id
 	FROM ' . USER_GROUP_TABLE . '
-	WHERE user_id = ' . (int) $user->data['user_id'] . ' 
+	WHERE user_id = ' . (int) $user->data['user_id'] . '
 	AND user_pending = 0';
 $result = $db->sql_query($sql);
 $in_group = $db->sql_fetchrowset($result);
 $db->sql_freeresult($result);
 
 // groups not displayed
-// you can add to the array if wanted 
+// you can add to the array if wanted
 // by adding the group name to ignore into the array
 // default group names are GUESTS REGISTERED REGISTERED_COPPA GLOBAL_MODERATORS ADMINISTRATORS BOTS
 $groups_not_display = array('GUESTS', 'BOTS');
@@ -43,7 +43,7 @@ if (!$config['coppa_enable'])
 {
 	$no_coppa = array('REGISTERED_COPPA');
 	$groups_not_display = array_merge($groups_not_display, $no_coppa);
-	
+
 	//free up a bit 'o memory
 	unset($no_coppa);
 }
@@ -56,7 +56,7 @@ $sql = 'SELECT *
 $result = $db->sql_query($sql);
 
 $group_rows = array();
-while ($row = $db->sql_fetchrow($result) ) 
+while ($row = $db->sql_fetchrow($result) )
 {
 	$group_rows[] = $row;
 }
@@ -67,32 +67,39 @@ $ranks = $cache->obtain_ranks();
 
 if ($total_groups = count($group_rows))
 {
-	// Obtain list of users of each group	
+	// Obtain list of users of each group
 	$sql = 'SELECT u.user_id, u.username, u.username_clean, u.user_colour, ug.group_id, ug.group_leader
 			FROM ' . USER_GROUP_TABLE . ' ug, ' . USERS_TABLE . ' u
 			WHERE ug.user_id = u.user_id
 			AND ug.user_pending = 0
 			AND u.user_id <> ' . ANONYMOUS . '
-			ORDER BY u.username';
+			ORDER BY  ug.group_leader DESC, u.username ASC';
 	$result = $db->sql_query($sql);
 
-	$group_users = array();			
+	$group_users = $group_leaders = array();
 	while ($row = $db->sql_fetchrow($result))
 	{
-		$group_users[$row['group_id']][] = get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']);
+		if ($row['group_leader'])
+		{
+			$group_leaders[$row['group_id']][] = get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']);
+		}
+		else
+		{
+			$group_users[$row['group_id']][] = get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']);
+		}
 	}
 	$db->sql_freeresult($result);
 
 	for($i = 0; $i < $total_groups; $i++)
 	{
-		$group_id = (int) $group_rows[$i]['group_id'];	
+		$group_id = (int) $group_rows[$i]['group_id'];
 		if ($group_rows[$i]['group_type'] == GROUP_HIDDEN && !$auth->acl_gets('a_group', 'a_groupadd', 'a_groupdel') && !in_array($group_id, $in_group[0]))
 		{
 			continue;
-		}	
-		
+		}
+
 		// Do we have a Group Rank?
-		
+
 		if ($group_rows[$i]['group_rank'])
 		{
 			if (isset($ranks['special'][$group_rows[$i]['group_rank']]))
@@ -120,14 +127,24 @@ if ($total_groups = count($group_rows))
 
 			'S_SHOW_RANK'			=> true,
 		));
-		
+
+		if (!empty($group_leaders[$group_id]))
+		{
+			foreach($group_leaders[$group_id] as $group_leader)
+			{
+				$template->assign_block_vars('groups.leaders', array(
+					'U_VIEW_PROFILE'			=> $group_leader,
+				));
+			}
+		}
+
 		if (!empty($group_users[$group_id]))
 		{
 			foreach($group_users[$group_id] as $group_user)
 			{
 				$template->assign_block_vars('groups.members', array(
 					'U_VIEW_PROFILE'			=> $group_user,
-				));		
+				));
 			}
 		}
 	}
